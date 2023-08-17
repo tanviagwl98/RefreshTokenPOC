7a0920940d9dc66efa1d129a9a330c95ba9e2b9d
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-08-17 13:06:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCu7mIvNfMs9iTM0vKxYF3Yyb0XAQXxlr0",
-  authDomain: "refreshtoken-a2387.firebaseapp.com",
-  projectId: "refreshtoken-a2387",
-  storageBucket: "refreshtoken-a2387.appspot.com",
-  messagingSenderId: "324924585806",
-  appId: "1:324924585806:web:9fe04fab8958afeca3901f",
-  measurementId: "G-KWFKDFF30B"
+  apiKey:"",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
